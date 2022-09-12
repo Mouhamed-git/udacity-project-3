a75f151559354eed85d359d7e544d695049920f8
Mouhamad DIACK <m.diackk@gmail.com>
2022-09-12 01:34:42 +0000

    first-commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Passer123.
+export POSTGRES_HOST=mdproject3-database.cehpzvlrrwpq.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=mdProject3Database
+export AWS_BUCKET=mdproject3-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=Project3Secret
+export URL=http://localhost:8100
